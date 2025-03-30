b522f2bb9f081c5cb27f128e23205079521ea955
joaqu1m <joaqu1m.pires@hotmail.com>
2025-03-30 01:53:09 +0000

    new bucket creatoin try

--- terraform/modules/n8n/new_test_bucket.tf
+++ terraform/modules/n8n/new_test_bucket.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "ci_cd_test_bucket" {
-  bucket = "n8n-bucket"
+  bucket = "n8n-bucket-unique-try"
 
   tags = {
-    Name        = "n8n-bucket"
+    Name        = "n8n-bucket-unique-try"
     Environment = "Test"
     Purpose     = "CI/CD Testing"
     CreatedAt   = formatdate("YYYY-MM-DD", timestamp())
