cbe7de847961c3a4a23893e9d92a69dec30fa0e2
joaqu1m <joaqu1m.pires@hotmail.com>
2025-03-31 02:43:37 +0000

    n8n ips http/https

--- terraform/modules/n8n/ec2_instance.tf
+++ terraform/modules/n8n/ec2_instance.tf
@@ -15,6 +15,22 @@ resource "aws_security_group" "n8n_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
