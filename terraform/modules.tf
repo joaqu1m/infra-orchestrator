2903bae3ddea8a3e7aca81d56b254c7e6ee388d7
joaqu1m <joaqu1m.pires@hotmail.com>
2025-04-05 01:46:45 +0000

    deleting dns config

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,7 +1,6 @@
 module "n8n" {
-  source          = "./modules/n8n"
-  ami_id          = var.ami_id
-  key_name        = var.key_name
-  iam_role_id     = var.iam_role_id
-  route53_zone_id = aws_route53_zone.iagiliza_zone.zone_id
+  source      = "./modules/n8n"
+  ami_id      = var.ami_id
+  key_name    = var.key_name
+  iam_role_id = var.iam_role_id
 }
--- terraform/modules/n8n/variables.tf
+++ terraform/modules/n8n/variables.tf
@@ -4,8 +4,6 @@ variable "ami_id" {}
 
 variable "iam_role_id" {}
 
-variable "route53_zone_id" {}
-
 variable "domain_name" {
   default = "n8n.iagiliza.com"
 }
